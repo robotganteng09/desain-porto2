5c58d54545b1cc6904cf3c05ad3534c522710ea2
robotganteng09 <arsyafaisyar@gmail.com>
2025-06-01 05:31:56 +0700

    aduuh

--- desain-porto/vite.config.js
+++ desain-porto/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { ghPages } from 'vite-plugin-gh-pages'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),ghPages(),
+    tailwindcss(),
   ],
   base: '/desain-porto/',
 })
